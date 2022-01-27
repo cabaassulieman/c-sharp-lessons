441b77c204c07ece8e7db6738cb4a7cacfa6b5e3
cabaas sulayman <cabaas.116@gmail.com>
2022-01-27 15:05:30 -0800

    c sharp lesson one

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int longNumber = int.MaxValue -2 ;
+Console.WriteLine("number is: " + longNumber );
+System.Console.WriteLine();
+
+
+
+var lyric = "the older i get, the harder i fall!";
+var result = lyric.Contains("older");
+System.Console.WriteLine("contains 'older' " + result);
+result = lyric.StartsWith("the");
+System.Console.WriteLine("starts with 'the'" +result);
+
+System.Console.WriteLine("total chars: " + lyric.Length);
